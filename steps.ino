38efab36312dbfc9523b21c57f0a23198512149a
vla-gove <107414426+vla-gove@users.noreply.github.com>
2022-11-14 03:15:26 +0100

    Add files via upload

---
+++ steps.ino
@@ -0,0 +1,6 @@
+//int  step1[servoCount] =  {x,  angle0,  x,   angle0,  x,  angle1,      x,    angle0};        
+//int  step2[servoCount] =  {x,  angle1,    x,    angle0,  x,  angle0,   x,   angle0};       
+//int  step3[servoCount] =  {x,  angle0, x,   angle0,  x,   angle0,    x,   angle1};         
+//int  step4[servoCount] =  {x,   angle0,  x,   angle1,   x,  angle0 ,   x,   angle0};       
+                        //front-right   //front-left     //rear-right       //rear-left
+                    //top/bottom servo //top/bottom servo //top/bottom servo //top/bottom servo

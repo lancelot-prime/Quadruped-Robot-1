c141c82a3bbe69276eecc4500ab8405c9de20101
vla-gove <107414426+vla-gove@users.noreply.github.com>
2022-11-20 01:17:28 +0100

    Delete steps.ino

--- steps.ino
+++
@@ -1,6 +0,0 @@
-//int  step1[servoCount] =  {x,  angle0,  x,   angle0,  x,  angle1,      x,    angle0};        
-//int  step2[servoCount] =  {x,  angle1,    x,    angle0,  x,  angle0,   x,   angle0};       
-//int  step3[servoCount] =  {x,  angle0, x,   angle0,  x,   angle0,    x,   angle1};         
-//int  step4[servoCount] =  {x,   angle0,  x,   angle1,   x,  angle0 ,   x,   angle0};       
-                        //front-right   //front-left     //rear-right       //rear-left
-                    //top/bottom servo //top/bottom servo //top/bottom servo //top/bottom servo
